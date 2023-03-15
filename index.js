fb9badebf0a7d24e95f0a5e9ac198502e6e96139
daniel <danielmutisya203@gmail.com>
2023-03-16 01:27:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+// Function to return the first two drivers from the passed-in array
+function returnFirstTwoDrivers(drivers) {
+  return drivers.slice(0, 2);
+}
+
+// Function to return the last two drivers from the passed-in array
+function returnLastTwoDrivers(drivers) {
+  return drivers.slice(-2);
+}
+
+// Array containing both functions that can be invoked
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+// Function to create a fare multiplier
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    };
+  }
+  
+  // Function to double fares
+  const fareDoubler = createFareMultiplier(2);
+  
+  // Function to triple fares
+  const fareTripler = createFareMultiplier(3);
+  
+  function selectDifferentDrivers(drivers, func) {
+    return func(drivers);
+  }
